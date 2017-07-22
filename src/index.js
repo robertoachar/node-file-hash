bf4698af9d8df2107413793224e666b3fdfa813f
Roberto Achar <robertoachar@gmail.com>
2017-07-22 18:53:12 -0300

    Adding lib

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-'use strict';
+const crypto = require('crypto');
 
-module.exports.hello = (done) => {
-  return done(null, 'It works!');
+module.exports.createHash = (buffer) => {
+  return new Promise((resolve, reject) => {
+    if (!buffer) reject('Buffer not found');
+
+    const md5 = crypto.createHash('md5').update(buffer).digest('hex');
+    const sha1 = crypto.createHash('sha1').update(buffer).digest('hex');
+    const sha256 = crypto.createHash('sha256').update(buffer).digest('hex');
+
+    const hash = {
+      md5,
+      sha1,
+      sha256
+    };
+
+    resolve(hash);
+  });
 };
